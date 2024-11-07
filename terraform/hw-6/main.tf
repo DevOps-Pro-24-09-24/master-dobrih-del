173e197997dac9ae5d33db2f41c10f4cc556bf7b
Maks <chempionmaks@gmail.com>
2024-11-07 18:19:31 +0100

    fix output web_instance_dns

--- terraform/hw-6/main.tf
+++ terraform/hw-6/main.tf
@@ -1,5 +1,7 @@
 resource "aws_vpc" "vpc-hw6" {
   cidr_block = var.cidr_pvc
+  enable_dns_support   = true
+  enable_dns_hostnames = true
   tags = {
     Name = "hw-6-vpc",
     env = "test",
@@ -27,4 +29,33 @@ resource "aws_subnet" "private" {
     env = "test",
     DZ = "hw6"
   }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc-hw6.id
+
+  tags = {
+    Name = "hw-6-igw",
+    env  = "test",
+    DZ   = "hw6"
+  }
+}
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.vpc-hw6.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "hw-6-public-rt",
+    env  = "test",
+    DZ   = "hw6"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
 }
